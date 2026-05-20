5b6c08c8bd182938dbb5d19cd5219a20c43dd996
Gaston <ggastlino@gmail.com>
2026-05-20 19:14:06 +0200

    feat(auth): implement Auth service with login method

--- frontend/src/app/features/auth/services/auth.ts
+++ frontend/src/app/features/auth/services/auth.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Api } from '../../../core/services/api';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+
+  constructor(private api: Api) {}
+
+  login(username: string, password: string) {
+    return this.api.post('/login', {
+      username,
+      password
+    });
+  }
+
+}
