5fa192f32e07b5c01745f74f333fdadc4df4f001
Gaston <ggastlino@gmail.com>
2026-05-20 21:42:59 +0200

    feat(dashboard): add loadCustomers method and button to fetch customer data

--- frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../../../auth/services/auth';
+import { Customer } from '../../../customers/services/customer';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,8 +14,20 @@ export class Dashboard {
 
   constructor(
     private auth: Auth,
-    private router: Router
+    private router: Router,
+    private customer: Customer
   ) {}
+
+  loadCustomers(): void {
+    this.customer.getAll().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 
   logout(): void {
 
