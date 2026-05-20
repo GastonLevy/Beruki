4a3432509eda3e497e681c8b5a0517dac1add890
Gaston <ggastlino@gmail.com>
2026-05-20 19:13:52 +0200

    feat(auth): update login form to use username instead of email

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +14,30 @@ export class Login {
   private fb = new FormBuilder();
 
   loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required]],
     password: ['', [Validators.required]]
   });
 
+  constructor(private auth: Auth) {}
+
   onSubmit(): void {
+
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
       return;
     }
 
-    console.log(this.loginForm.value);
+    const { username, password } = this.loginForm.getRawValue();
+
+    this.auth.login(username!, password!).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+
   }
 
 }
