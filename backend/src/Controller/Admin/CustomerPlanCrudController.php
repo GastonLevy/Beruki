49970abc5fffc1261403104f4e2b64e00c2ba568
Gastón Levy <ggastlino@gmail.com>
2026-06-02 14:31:23 +0200

    feat(customer-plan-crud): enhance customer and plan queries to exclude archived entries

--- backend/src/Controller/Admin/CustomerPlanCrudController.php
+++ backend/src/Controller/Admin/CustomerPlanCrudController.php
@@ -3,8 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\CustomerPlan;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -28,15 +33,41 @@ class CustomerPlanCrudController extends AbstractCrudController
             ->setPageTitle(Crud::PAGE_DETAIL, 'Detalle del plan de cliente');
     }
 
+    public function createIndexQueryBuilder(
+        SearchDto $searchDto,
+        EntityDto $entityDto,
+        FieldCollection $fields,
+        FilterCollection $filters
+    ): QueryBuilder {
+        $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+        $queryBuilder
+            ->join('entity.customer', 'customer')
+            ->andWhere('customer.isArchived = :isArchived')
+            ->setParameter('isArchived', false);
+
+        return $queryBuilder;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id')
                 ->hideOnForm(),
 
-            AssociationField::new('customer', 'Cliente'),
+            AssociationField::new('customer', 'Cliente')
+                ->setQueryBuilder(
+                    fn (QueryBuilder $queryBuilder): QueryBuilder => $queryBuilder
+                        ->andWhere('customer.isArchived = :isArchived')
+                        ->setParameter('isArchived', false)
+                ),
 
-            AssociationField::new('plan', 'Plan'),
+            AssociationField::new('plan', 'Plan')
+                ->setQueryBuilder(
+                    fn (QueryBuilder $queryBuilder): QueryBuilder => $queryBuilder
+                        ->andWhere('plan.isActive = :isActive')
+                        ->setParameter('isActive', true)
+                ),
 
             DateTimeField::new('startedAt', 'Fecha de inicio'),
 
